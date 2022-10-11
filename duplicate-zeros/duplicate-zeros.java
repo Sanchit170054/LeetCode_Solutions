9707e7cf0c00451639bfe1566ec437d3e8bd3e2e
Sanchit <34150437+Sanchit170054@users.noreply.github.com>
2022-10-12 00:38:26 +0530

    Time: 3 ms, Memory: 46.6 MB - LeetHub

---
+++ duplicate-zeros/duplicate-zeros.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void duplicateZeros(int[] arr) {
+     Queue<Integer> q = new LinkedList<>();
+        for(int i = 0; i < arr.length; i++)
+        {
+            if(arr[i] == 0)
+            {
+                q.add(0);
+                q.add(0);
+            }
+            else
+            {
+                q.add(arr[i]);
+            }
+            
+            arr[i] = q.poll();
+        }
+    }
+}
